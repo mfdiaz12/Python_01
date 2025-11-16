8dfc705b3fa98f11655f1fa741f0a46749ba878e
mfdiaz12 <manuelfran@MacBook-Pro-de-Manuel.local>
2025-11-16 16:16:00 -0500

    Condicionales

--- Clase Virtual_02/01_if.py
+++ Clase Virtual_02/01_if.py
@@ -40,11 +40,40 @@ else:
     print("¡No estas calificado!")
 
 print("\n Condiciones multiples")
-edad = 25
+edad = 17
 tiene_carnet = True
 
-if edad >= 18 and tiene_carnet:
+if edad >= 18 and tiene_carnet: # Tiene que cumplir las 2 condiciones para que sea true
     print("Puede manejar🚗")
 else:
     print("POLICIA 🚓")
-    
+
+if edad >= 18 or tiene_carnet: # Con que una de las 2 condiciones se cumpla ya la sentencia es true
+    print("Puedes conducir en la Isla Margarita🚗")
+else:
+    print("Paga al policia y de deja conducir")
+
+es_fin_de_semana = False
+if not es_fin_de_semana: # El not nos sirve para darle la vuelta a la condición si cambia la condicion a True no imprime nada
+    print("Venga vamos a trabajar")
+
+print("\n Anidar condicionales") # Poner un condicional dentro de otro, a mas condicionales internos mas complejo es el codigo.
+edad = 20 # lo mprimero que evalua es si tienes la edad y luego si tienes dinero.
+tiene_dinero = True
+
+if edad >= 18:
+    if tiene_dinero:
+        print("Puedes ir a la farra")
+    else:
+        print("Quedate en casa")
+else:
+    print("No puedes entrar a la disco")
+
+# Intentar simplificar, es este caso seria asi mas facil
+ 
+if edad <= 18:
+    print("No pueds ir a la farra")
+elif tiene_dinero:
+    print("Puedes ir a la discoteca")
+else:
+    print("Quedate en casa")
