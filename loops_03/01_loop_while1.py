eac5a9c72857efde841bbd033b89292ab1b5fd23
mfdiaz12 <106556431+mfdiaz12@users.noreply.github.com>
2025-12-15 23:52:36 -0500

    Add files via upload

---
+++ loops_03/01_loop_while1.py
@@ -0,0 +1,12 @@
+### 
+# 01 - Bucle (while)
+# Permite ejecutar un bloque de código repetidamente mientras se cumpla una condición
+###
+
+print("\n Bucle while:")
+
+# Bucle con una simple condición
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1 # Esto es super importante para evitar un bucle infinito
